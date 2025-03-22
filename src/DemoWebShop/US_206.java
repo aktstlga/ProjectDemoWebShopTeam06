847cc552508a49c66e18acd15b2bf7105790e81f
AktsT <akts.tlga@gmail.com>
2025-03-23 01:53:39 +0300

    Driver.get and Actions/Wait objects are created. logInToAccount method is created.

--- src/DemoWebShop/US_206.java
+++ src/DemoWebShop/US_206.java
@@ -1,4 +1,25 @@
 package DemoWebShop;
 
-public class US_206 {
+import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class US_206 extends BaseDriver {
+    @Test
+    public void logInToAccount(){
+        driver.get("https://demowebshop.tricentis.com");
+
+        Actions actionDriver=new Actions(driver);
+        WebDriverWait waitDuration = new WebDriverWait(driver, Duration.ofSeconds(120));
+
+
+
+    }
 }
