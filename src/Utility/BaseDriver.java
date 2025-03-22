35184ede2c42afe49c59dcfbcd589584ae96f04f
AktsT <akts.tlga@gmail.com>
2025-03-23 01:47:28 +0300

    Myfunc deleted, BaseDriver edited.

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -15,10 +15,6 @@ public class BaseDriver{
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
-    public static void WaitClose() {
-        Myfunc.wait(2);
-        driver.quit();
-    }
     public static void Driver(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
