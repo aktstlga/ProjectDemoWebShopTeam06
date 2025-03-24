10706719d2c9cdcd1bf6b9240a13573fc0707a89
meertsarac <meertsarac@gmail.com>
2025-03-24 04:12:21 +0300

    Identified login button by class name and added click action.

--- src/DemoWebShop/US_204.java
+++ src/DemoWebShop/US_204.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+
 public class US_204 extends BaseDriver {
     @Test
     public void Login() {
         driver.get("https://demowebshop.tricentis.com/");
+
+        WebElement login = driver.findElement(By.className("ico-login"));
+        login.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.quit();
 
 
     }
