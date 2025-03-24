12e0831d8a1ae293ee3764b7a8cef7caaccc797d
meertsarac <meertsarac@gmail.com>
2025-03-24 04:50:59 +0300

    Identified "login button click" by xpath name and added click action.

--- src/DemoWebShop/US_204.java
+++ src/DemoWebShop/US_204.java
@@ -16,12 +16,16 @@ public class US_204 extends BaseDriver {
         login.click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        WebElement emailPlaceholder=driver.findElement(By.id("Email"));
+        WebElement emailPlaceholder = driver.findElement(By.id("Email"));
         emailPlaceholder.sendKeys("team006test@gmail.com");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement passwordPlaceHolder=driver.findElement(By.cssSelector("[type='password']"));
+        WebElement passwordPlaceHolder = driver.findElement(By.cssSelector("[type='password']"));
         passwordPlaceHolder.sendKeys("Password123");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+        loginButton.click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         driver.quit();
