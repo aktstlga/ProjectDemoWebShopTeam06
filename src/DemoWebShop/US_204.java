a8e1af43ab49a076c172a6e43b203c653ec8f441
meertsarac <meertsarac@gmail.com>
2025-03-24 03:11:17 +0300

    Added website navigation.

--- src/DemoWebShop/US_204.java
+++ src/DemoWebShop/US_204.java
@@ -2,10 +2,14 @@ package DemoWebShop;
 
 import Utility.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class US_204 extends BaseDriver {
     @Test
     public void Login() {
+        driver.get("https://demowebshop.tricentis.com/");
+
 
     }
 }
