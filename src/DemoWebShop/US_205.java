7a43dafebd97e8f9d3dda17dc6ead3387afc9729
mert <mertcanat91@gmail.com>
2025-03-25 18:55:37 +0300

    US_205 PleaseFillTheMissingField tamamlandı

--- src/DemoWebShop/US_205.java
+++ src/DemoWebShop/US_205.java
@@ -6,33 +6,34 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class US_205 extends BaseDriver {
+    WebDriverWait waitDuration = new WebDriverWait(driver, Duration.ofSeconds(120));
 
     @Test
     public void PleaseFillTheMissingField(){
         driver.get("https://demowebshop.tricentis.com/");
 
         WebElement login = driver.findElement(By.className("ico-login"));
-        login.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
+        waitForVisibilityAndClickThanClick(login);
+
+        waitDuration.until(ExpectedConditions.urlMatches("https://demowebshop.tricentis.com/login"));
 
         WebElement email = driver.findElement(By.className("email"));
-        email.sendKeys("");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
+        fillingThePlaceholderWithWait(email, "");
 
-        WebElement password = driver.findElement(By.cssSelector("input[type='password']"));
-        password.sendKeys("");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
+        WebElement password = driver.findElement(By.className("password"));
+        fillingThePlaceholderWithWait(password,"");
 
-        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit']"));
-        submitButton.click();
+        WebElement submitButton = driver.findElement(By.cssSelector("input[value='Log in'][type='submit']"));
+        waitForVisibilityAndClickThanClick(submitButton);
 
-        WebElement demoWebShop = driver.findElement(By.cssSelector("div[class='header-logo]'"));
-        Assert.assertTrue("Aranan mesaj bulunamadı",demoWebShop.getText().contains("Demo Web Shop"));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
+        waitDuration.until(ExpectedConditions.urlMatches("https://demowebshop.tricentis.com"));
+        Assert.assertTrue("Aranan mesaj bulunamadı.",driver.getCurrentUrl().contains("https://demowebshop.tricentis.com"));
 
         driver.quit();
     }
