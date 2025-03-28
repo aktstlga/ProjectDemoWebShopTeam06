b4a440d36e0d0906dc5a9c4269ce14be50831672
Msı <berkgultekin66@gmail.com>
2025-03-28 22:33:45 +0300

    US_209 logın.

--- src/DemoWebShop/US_209.java
+++ src/DemoWebShop/US_209.java
@@ -3,11 +3,19 @@ package DemoWebShop;
 import Utility.BaseDriver;
 import com.google.common.annotations.VisibleForTesting;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class US_209 extends BaseDriver {
     @Test
     public void US_209(){
         driver.get("https://demowebshop.tricentis.com/");
+        WebElement logınbutton= driver.findElement(By.xpath("//a[@class='ico-login']"));
+        wait.until(ExpectedConditions.visibilityOf(logınbutton));
+        logınbutton.click();
+
 
     }
 }
