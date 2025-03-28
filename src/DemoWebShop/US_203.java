0b8e5e4573f64f3c27f9ed43ceba37b5fe98ea8c
AktsT <akts.tlga@gmail.com>
2025-03-28 20:58:27 +0300

    Adjusted for Arif cause he got some error.

--- src/DemoWebShop/US_203.java
+++ src/DemoWebShop/US_203.java
@@ -13,19 +13,14 @@ import org.openqa.selenium.support.ui.Wait;
 public class US_203 extends BaseDriver {
     @Test
     public void LogOut(){
-
         driver.get("https://demowebshop.tricentis.com/");
-        Myfunc.wait(2);
-
         loginMethodAccordingToUs_204();
 
         WebElement btnnLogOut = driver.findElement(By.xpath("//*[@class='ico-logout']"));
-        Actions action111 = new Actions(driver);
-        action111.moveToElement(btnnLogOut).click().build().perform();
+        actionDriver.moveToElement(btnnLogOut).click().build().perform();
 
         WebElement btnLogin = driver.findElement(By.xpath("//*[@class='ico-login']"));
         Assert.assertTrue("Ana Sayfaya Dönüş Olmadı.",btnLogin.getText().contains("Log in"));
-        System.out.println("ANA SAYFAYA BAŞARILI BİR ŞEKİLDE DÖNÜŞ OLDU.");
-
+        System.out.println("Ana sayfaya başarılı dönüş oldu.");
     }
 }
