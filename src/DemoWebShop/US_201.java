cad47fa5893c89b802ea49511f0ac06eaf77ced5
Baris Sancar <bariss.sancarr@mail.com>
2025-04-05 20:49:31 +0300

    gender selected button added and clickable

--- src/DemoWebShop/US_201.java
+++ src/DemoWebShop/US_201.java
@@ -4,20 +4,20 @@ import Utility.BaseDriver;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class US_201 extends BaseDriver {
 
     @Test
     public void registerUser() {
-    driver.get("https://demowebshop.tricentis.com/");
+        driver.get("https://demowebshop.tricentis.com/");
         WebElement register = driver.findElement(By.className("ico-register"));
         waitForVisibilityAndClickThanClick(register);
 
         wait.until(ExpectedConditions.urlMatches("https://demowebshop.tricentis.com/register"));
 
-
+        WebElement gender = driver.findElement(By.id("gender-male"));
+        waitForVisibilityAndClickThanClick(gender);
 
     }
 
