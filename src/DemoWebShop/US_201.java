091014206c16fc4371b426ca1af6ddf6445a5dfc
Baris Sancar <bariss.sancarr@mail.com>
2025-04-05 20:19:10 +0300

    Register button created

--- src/DemoWebShop/US_201.java
+++ src/DemoWebShop/US_201.java
@@ -2,11 +2,17 @@ package DemoWebShop;
 
 import Utility.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class US_201 extends BaseDriver {
 
     @Test
     public void registerUser() {
+    driver.get("https://demowebshop.tricentis.com/");
+        WebElement register = driver.findElement(By.className("ico-register"));
+        waitForVisibilityAndClickThanClick(register);
+
 
     }
 
